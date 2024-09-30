d70dd80a4464f02617dc0e2dadd4739874347874
vin <vin@vin.vin>
2024-09-30 11:14:35 +0530

    buttons added

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
+const messageEl= document.querySelector("#message-el");
+const sumEl= document.querySelector("#sum-el");
+const cardsEl= document.querySelector("#cards-el");
+
 let firstCard= 11;
-let secondCard= 9;
+let secondCard= 10;
 
 let sum = firstCard + secondCard;
 let hasBlackjack = false;
 let isAlive=true;
 let message = "";
 
-if (sum <=20){
-    message = "Do you want a new card? 🙂";
-}else if(sum ===21){
-    message= "Blackjack! 🥳"
-    hasBlackjack= true;
-}else{
-    message= "You're out of game! 😭";
-    isAlive=false;
+
+function startGame(){
+    cardsEl.textContent= `Cards: ${firstCard} ${secondCard}`;
+    sumEl.textContent= `Sum: ${sum}`;
+    if (sum <=20){
+        message = "Do you want a new card?";
+
+    }else if(sum ===21){
+        message= "You've got Blackjack!"
+
+        hasBlackjack= true;
+    }else{
+        message= "You're out of game!";
+
+        isAlive=false;
+    }
+    messageEl.textContent= message;
 }
-console.log(message);
+
+function newCard(){
+    console.log("drawing new card");
+}
